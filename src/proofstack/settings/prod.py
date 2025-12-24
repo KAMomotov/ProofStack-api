f5b097a3760efe6d447a07dfd8498ab6cf31451c
Kirill Momotov <unknownman@live.ru>
2025-12-24 11:03:42 +0200

    entrypoint.sh src/proofstack/prod.py

--- src/proofstack/settings/prod.py
+++ src/proofstack/settings/prod.py
@@ -5,3 +5,4 @@ DEBUG = False
 # полезно для ingress-nginx:
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 USE_X_FORWARDED_HOST = True
+CSRF_TRUSTED_ORIGINS = ["https://proofstack.local"]
